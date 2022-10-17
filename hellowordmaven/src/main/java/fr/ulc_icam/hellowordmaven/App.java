0822ff6ea56353ea95c253101167e2fccf0d5b10
El Adri <El Adri@DESKTOP-78GK6N2>
2022-10-17 11:28:19 +0100

    Ceci est un hello word de notre CE.

---
+++ hellowordmaven/src/main/java/fr/ulc_icam/hellowordmaven/App.java
@@ -0,0 +1,13 @@
+package fr.ulc_icam.hellowordmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
